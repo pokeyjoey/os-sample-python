7ee8d1f8494398dd7f552005a36383288a312a05
Roche <D1JRR01@rb.win.frb.org>
2020-03-04 10:29:46 -0500

    Change message displayed for Flask application

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "This is the way the world ends, not with a bang .... but a whimper."
 
 if __name__ == "__main__":
     application.run()
